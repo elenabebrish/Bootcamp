01c174a540c89e69f51900b3c582673431111c6d
Elena Bebrish <bebrish.elena@gmail.com>
2022-06-28 16:32:29 +0300

    Tasks 1-6

---
+++ src/task1.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+
+        boolean x = false;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter value A: ");
+        Integer input = scanner.nextInt();
+        System.out.println("Enter value B: ");
+        Integer input2 = scanner.nextInt();
+
+        if(input == input2) {
+            x = true;
+            System.out.println("Is " + input + " equal to " + input2 + "? " + x);
+        }else{
+            System.out.println("Is " + input + " equal to " + input2 + "? " + x);
+        }
+
+        if(input < input2) {
+            x = true;
+            System.out.println("Is " + input + " less than " + input2 + "? " + x);
+            }else{
+            System.out.println("Is " + input + " less than " + input2 + "? " + x);
+
+        if(input <= input2) {
+            x = true;
+            System.out.println("Is " + input + " less or equal to " + input2 + "? " + x);
+            } else {
+            System.out.println("Is " + input + " less or equal to " + input2 + "? " + x);
+        }
+
+        if(input > input2){
+            x = true;
+            System.out.println("Is " + input + " greater than " + input2 + "? " + x);
+        } else {
+            System.out.println("Is " + input + " greater than " + input2 + "? " + x);
+        }
+
+        if(input >= input2) {
+            x = true;
+            System.out.println("Is " + input + " greater or equal to " + input2 + "? " + x);
+        }else{
+            System.out.println("Is " + input + " greater or equal to " + input2 + "? " + x);
+        }
+
+
+
+        }
+
+    }
+}
